cb3c1d528209ca9deb44772bf07ee232dc312fce
WJZ_P <110795301+WJZ-P@users.noreply.github.com>
2025-08-26 00:54:30 +0800

    增加rememberKeyArrayState

--- app/src/main/java/me/wjz/nekocrypt/ui/dialog/KeyManagementDialog.kt
+++ app/src/main/java/me/wjz/nekocrypt/ui/dialog/KeyManagementDialog.kt
@@ -1,13 +1,12 @@
 package me.wjz.nekocrypt.ui.dialog
 
 import androidx.compose.runtime.Composable
-import me.wjz.nekocrypt.Constant
-import me.wjz.nekocrypt.SettingKeys.ALL_THE_KEYS
-import me.wjz.nekocrypt.hook.rememberDataStoreState
+import androidx.compose.runtime.getValue
+import me.wjz.nekocrypt.data.rememberKeyArrayState
 
 @Composable
 fun KeyManagementDialog(onDismissRequest: () -> Unit) {
 
     // 状态管理
-    val keys  by rememberDataStoreState(ALL_THE_KEYS, Constant.DEFAULT_SECRET_KEY)
+    val keys: Array<String> by rememberKeyArrayState()
 }
